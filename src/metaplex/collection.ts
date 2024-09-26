1ad70cd8adc935e5d055f7925ccc9b4cdeff83e7
Suvijak (Guy) <guy.suvijak@hotmail.com>
2024-09-26 22:07:26 +0700

    updated

--- src/metaplex/collection.ts
+++ src/metaplex/collection.ts
@@ -1,6 +1,5 @@
 import { TransactionBuilderSendAndConfirmOptions, generateSigner, publicKey, signerIdentity, createSignerFromKeypair } from '@metaplex-foundation/umi';
-import { createCollection, fetchCollection, updateCollection, ruleSet } from '@metaplex-foundation/mpl-core';
-
+import { createCollection, fetchCollection, ruleSet } from '@metaplex-foundation/mpl-core';
 import { umi } from '@/utils/umi';
 
 const secret = new Uint8Array([16,151,34,252,65,187,220,138,128,20,165,199,238,236,209,57,53,216,182,163,172,121,252,108,93,254,189,205,158,110,212,181,215,25,187,26,229,167,110,10,19,111,134,18,169,32,209,221,24,193,47,208,204,33,202,197,73,51,151,10,204,181,23,136]);
@@ -9,7 +8,7 @@ const collectionSigner = createSignerFromKeypair(umi, myKeypair);
 umi.use(signerIdentity(collectionSigner));
 
 const creator1 = publicKey('HQx4BtM2QuGHg3RWmd1axx5JxMj7t5UDzhcm1fosm1uH');
-const collectionName = 'StarryCiels Planet Test1';
+const collectionName = 'StarryCiels Planet';
 const collectionAddress = publicKey('Abku8gXwJqxhV2tDnffUnkUGjqdbsi3WWEQhZf2LTwuQ');
 
 export const CreateCollection = async () => {
@@ -30,7 +29,7 @@ export const CreateCollection = async () => {
                 "name": collectionName
             },
             "description": "StarryCiels is an experimental pixel art collection that has all of its art assets stored directly on the solana blockchain.",
-            "image": `https://starryciels.vercel.app/metadata/icon-512x512.png`,
+            "image": `https://starryciels.vercel.app/assets/planet-collection.png`,
             "name": collectionName,
             "properties": {
                 "category": "image",
@@ -44,7 +43,7 @@ export const CreateCollection = async () => {
                 "files": [
                     {
                         "type":"image/png",
-                        "uri": `https://starryciels.vercel.app/metadata/icon-512x512.png`,
+                        "uri": `https://starryciels.vercel.app/assets/planet-collection.png`,
                     }
                 ]
             },
@@ -52,12 +51,15 @@ export const CreateCollection = async () => {
             "symbol": ""
         }
     ];
+    const metadataString = JSON.stringify(collectionUri);
+    const metadataBase64 = Buffer.from(metadataString).toString('base64');
+    const base64Uri = `data:application/json;base64,${metadataBase64}`;
 
     const response = await createCollection(umi, {
         collection: signer,
         updateAuthority: collectionSigner.publicKey,
         name: collectionName,
-        uri: `https://starryciels.vercel.app/metadata`,
+        uri: base64Uri,
         plugins: [
             {
                 type: 'Royalties',
@@ -106,12 +108,3 @@ export const FetchCollection = async () => {
     const response = await fetchCollection(umi, collectionAddress);
     return response;
 };
-
-//export const UpdateCollection = async () => {
-//    const response = await updateCollection(umi, {
-//        collection: collectionAddress,
-//        name: collectionName,
-//        uri: `data:application/json;base64,${Buffer.from(JSON.stringify(collectionUri[0])).toString('base64')}`,
-//    }).sendAndConfirm(umi);
-//    return response;
-//};
---
+++ src/metaplex/planet.ts
@@ -0,0 +1,95 @@
+import { TransactionBuilderSendAndConfirmOptions, publicKey, generateSigner, createSignerFromKeypair, signerIdentity } from '@metaplex-foundation/umi';
+import { fetchCollection, create, fetchAssetsByOwner, update, fetchAsset, updateAuthority } from '@metaplex-foundation/mpl-core';
+import { umi } from '@/utils/umi';
+
+const addressProfileCollection = publicKey('7N1e73MEwJ1saYauQWWNZ81n3xuzXr126KrPiHGAQjad');
+const addressUpdated = publicKey('FUfQsR8QnCEUd1ZbzvCSCkNbnS1aCHKpPGeAYptKrtsZ');
+
+const txConfig: TransactionBuilderSendAndConfirmOptions = {
+    send: { skipPreflight: true },
+    confirm: { commitment: 'processed' },
+};
+
+const secret = new Uint8Array([16,151,34,252,65,187,220,138,128,20,165,199,238,236,209,57,53,216,182,163,172,121,252,108,93,254,189,205,158,110,212,181,215,25,187,26,229,167,110,10,19,111,134,18,169,32,209,221,24,193,47,208,204,33,202,197,73,51,151,10,204,181,23,136]);
+const myKeypair = umi.eddsa.createKeypairFromSecretKey(secret);
+const collectionSigner = createSignerFromKeypair(umi, myKeypair);
+umi.use(signerIdentity(collectionSigner));
+
+export const CreateProfile = async (owner: string, nickname: string) => {
+    const assetSigner = generateSigner(umi);
+
+    const collection = await fetchCollection(umi, addressProfileCollection);
+    const currentDate = new Date().toISOString();
+
+    const attributes = [
+        { trait_type: "birthday", value: currentDate },
+        { trait_type: "nickname", value: nickname }
+    ];
+
+    const metadata = {
+        "name": "Player Profile",
+        "symbol": "STCTPF",
+        "description": "StarryCiels Player Profile",
+        "image": "https://starryciels.vercel.app/assets/metaplex/profile.png",
+        "external_url": "https://starryciels.vercel.app",
+        "attributes": attributes
+    };
+    const metadataString = JSON.stringify(metadata);
+    const metadataBase64 = Buffer.from(metadataString).toString('base64');
+    const base64Uri = `data:application/json;base64,${metadataBase64}`;
+
+    const response = await create(umi, {
+        name: 'Player Profile',
+        uri: base64Uri,
+        asset: assetSigner,
+        owner: publicKey(owner),
+        collection: collection,
+        plugins: [
+            {
+                type: 'TransferDelegate',
+                authority: { type: 'Address', address: addressProfileCollection },
+            },
+            {
+                type: 'UpdateDelegate',
+                authority: { type: 'Address', address: addressProfileCollection },
+                additionalDelegates: [
+                    addressUpdated
+                ]
+            },
+            {
+                type: 'Attributes',
+                attributeList: [
+                    { key: "birthday", value: currentDate },
+                    { key: "nickname", value: nickname }
+                ]
+            }
+        ],
+    }).sendAndConfirm(umi, txConfig);
+    return { response, assetAddress: assetSigner.publicKey };
+};
+
+export const CheckProfile = async (owner: string) => {
+    const assetsByOwner = await fetchAssetsByOwner(umi, owner, {
+        skipDerivePlugins: false,
+    })
+    const collectionAssets = assetsByOwner.filter((asset: any) => 
+        asset.updateAuthority.type === 'Collection' && asset.updateAuthority.address.toString() === addressProfileCollection.toString()
+    );
+    return collectionAssets;
+};
+
+export const UpdateProfile = async (owner: string, assetId: any) => {
+    const asset = await fetchAsset(umi, assetId);
+    const base64Data = asset.uri.split(',')[1];
+    const jsonString = atob(base64Data);
+    const metadata = JSON.parse(jsonString);
+    const updatedJsonString = JSON.stringify(metadata);
+    const updatedBase64 = btoa(updatedJsonString);
+    const newUri = `data:application/json;base64,${updatedBase64}`;
+    const response = await update(umi, {
+        asset: asset,
+        name: 'Player Profile',
+        uri: newUri
+    }).sendAndConfirm(umi, txConfig);
+    return response;
+};
